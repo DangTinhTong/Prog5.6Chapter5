90319e8cbfcca99ddc5d3a9a4e230848b267803a
admin <dangtinhtong@gmail.com>
2017-11-24 23:34:53 +0700

    firs commit

--- Prog5.6/main.m
+++ Prog5.6/main.m
@@ -2,16 +2,24 @@
 //  main.m
 //  Prog5.6
 //
-//  Created by admin on 11/24/17.
-//  Copyright © 2017 admin. All rights reserved.
+//  Created by Tống Đăng Tình on 11/24/17.
+//  Copyright © 2017 Tống Đăng Tình. All rights reserved.
 //
+// This program introduction the while statement
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        
+        int count =1;
+        while(count<=5)  // Neu ko co dau {} thi no se la vong lap vo tan
+        {
+            NSLog(@"%i" , count);
+            ++count;
+        }
     }
     return 0;
 }
